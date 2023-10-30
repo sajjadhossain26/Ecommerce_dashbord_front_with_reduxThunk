a2d888ae3659f32f4dd66e858359ae4adc8a3b65
unknown <sajjadhossainctg26@gmail.com>
2023-10-30 14:19:34 +0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,45 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/router'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import Cookies from 'js-cookie'
-import { useDispatch } from 'react-redux';
-import { loggedInUser } from './features/auth/authApiSlice';
-import { getAllpermission, getAllrole } from './features/user/userApiSlice';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { loggedInUser } from "./features/auth/authApiSlice";
+import { getAllpermission, getAllrole } from "./features/user/userApiSlice";
 
 function App() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if(localStorage.getItem("user")){
-
-      dispatch(loggedInUser())
+    if (localStorage.getItem("user")) {
+      dispatch(loggedInUser());
     }
-  }, [dispatch])
+  }, [dispatch]);
 
   useEffect(() => {
-    dispatch(getAllpermission())
-    dispatch(getAllrole())
-  },[])
+    dispatch(getAllpermission());
+    dispatch(getAllrole());
+  }, [dispatch]);
 
   return (
     <>
- <ToastContainer
-    position="top-center"
-    autoClose={3000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss={false}
-    draggable
-    pauseOnHover={false}
-    theme="light"
-    />
-     <RouterProvider router={router}/>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="light"
+      />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
